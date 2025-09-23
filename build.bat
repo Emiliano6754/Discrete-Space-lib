c8cbf52789715268fbe861765d118149ce44806c
Emiliano Pastrana <emilianopm93@gmail.com>
2025-09-23 11:07:17 -0600

    Created Discrete Space library from common functionality

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+g++ -c -O3 -ffast-math -march=native -pthread -fopenmp -std=c++23 -mavx2 src/GF2N.cpp -Iinclude -o build/GF2N.o
+
+g++ -c -I "C:\\dev\\lib\\eigen" -O3 -ffast-math -march=native -pthread -fopenmp -std=c++23 src/sym_space.cpp -Iinclude -o build/sym_space.o
+
+g++ -c -I "C:\\dev\\lib\\eigen" -O3 -ffast-math -march=native -pthread -fopenmp -std=c++23 src/Qfunc.cpp -Iinclude -o build/Qfunc.o
+
+g++ -c -I "C:\\dev\\lib\\eigen" -O3 -ffast-math -march=native -pthread -fopenmp -std=c++23 src/displaced_Qfunc.cpp -Iinclude -o build/displaced_Qfunc.o
+
+g++ -c -I "C:\\dev\\lib\\eigen" -O3 -ffast-math -march=native -pthread -fopenmp -std=c++23 src/states.cpp -Iinclude -o build/states.o
+
+ar rcs build/libdiscrete_space.a build/GF2N.o build/sym_space.o build/Qfunc.o build/displaced_Qfunc.o build/states.o
+
+pause
+
+endlocal
