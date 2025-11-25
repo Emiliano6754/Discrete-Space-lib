2f03e00142458b3ae561b587c5240ca7ff60535c
Emiliano Pastrana <emilianopm93@gmail.com>
2025-11-25 09:21:37 -0600

    Added Qfunc overload for complex states

--- include/Qfunc.h
+++ include/Qfunc.h
@@ -4,8 +4,11 @@
 #include<Eigen/Dense>
 #include<complex>
 
-// Calculates the discrete Q function of a pure state from its operational basis expansion
+// Calculates the discrete Q function of a pure state from its operational basis expansion, assuming the state has no imaginary part
 Eigen::MatrixXd pure_Qfunc_from_operational(const unsigned int &n_qubits, const unsigned int &qubitstate_size, const Eigen::VectorXd &state);
+
+// Calculates the discrete Q function of a pure state from its operational basis expansion
+Eigen::MatrixXd pure_Qfunc_from_operational(const unsigned int &n_qubits, const unsigned int &qubitstate_size, const Eigen::VectorXcd &state);
 
 // Prints the Qfunc to console as a function of alpha and beta for debugging purposes 
 void print_Qfunc(const Eigen::MatrixXd &Qfunc);
--- src/Qfunc.cpp
+++ src/Qfunc.cpp
@@ -11,7 +11,7 @@ inline void initialize_xi_buffer(const unsigned int &n_qubits, const std::comple
     }
 } 
 
-// Calculates the discrete Q function of a pure state from its operational basis expansion
+// Calculates the discrete Q function of a pure state from its operational basis expansion, assuming the state has no imaginary part
 Eigen::MatrixXd pure_Qfunc_from_operational(const unsigned int &n_qubits, const unsigned int &qubitstate_size, const Eigen::VectorXd &state) {
     Eigen::MatrixXd Qfunc(qubitstate_size, qubitstate_size);
     const std::complex<double> xi = 0.5 * (sqrt(3)-1) * std::complex<double>(1.0,1.0);
@@ -36,6 +36,33 @@ Eigen::MatrixXd pure_Qfunc_from_operational(const unsigned int &n_qubits, const 
     }
     return Qfunc;
 }
+
+// Calculates the discrete Q function of a pure state from its operational basis expansion
+Eigen::MatrixXd pure_Qfunc_from_operational(const unsigned int &n_qubits, const unsigned int &qubitstate_size, const Eigen::VectorXcd &state) {
+    Eigen::MatrixXd Qfunc(qubitstate_size, qubitstate_size);
+    const std::complex<double> xi = 0.5 * (sqrt(3)-1) * std::complex<double>(1.0,1.0);
+    const double norm = 1.0 / std::pow((1 + std::norm(xi)), n_qubits);
+    std::complex<double>* xi_conj_buffer = static_cast<std::complex<double>*>( _malloca((n_qubits + 1) * sizeof(std::complex<double>)) );
+    initialize_xi_buffer(n_qubits, std::conj(xi), xi_conj_buffer);
+    
+    #pragma omp parallel
+    {
+        std::complex<double> sum;
+
+        #pragma omp for collapse(2)
+        for (unsigned int alpha = 0; alpha < qubitstate_size; alpha++) {
+            for (unsigned int beta = 0; beta < qubitstate_size; beta++) {
+                sum = 0;
+                for (unsigned int eta = 0; eta < qubitstate_size; eta++) {
+                    sum += (1.0 - 2 * std::popcount(alpha & eta)) * xi_conj_buffer[std::popcount(beta ^ eta)] * state[eta];
+                }
+                Qfunc(alpha, beta) = norm * std::norm(sum);
+            }
+        }
+    }
+    return Qfunc;
+}
+
 
 // Prints the Qfunc to console as a function of alpha and beta for debugging purposes 
 void print_Qfunc(const Eigen::MatrixXd &Qfunc) {
