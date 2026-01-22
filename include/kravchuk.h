a904e62179ba1d2d6abf358a77af1ea5fc3cb356
Emiliano Pastrana <emilianopm93@gmail.com>
2026-01-22 15:03:18 -0600

    Optimized Kravchuk batch calculation to avoid copying

--- include/kravchuk.h
+++ include/kravchuk.h
@@ -10,6 +10,12 @@ public:
     polynomial(unsigned int const &rank, std::unique_ptr<int[]> const &in_coeffs);
     // Copy constructor
     polynomial(polynomial const &other);
+    //  Builds the next Kravchuk polynomial from Kravchuk recurrence relations
+    polynomial(polynomial const &first, polynomial const &second, unsigned int const &N): n_rank(second.rank() + 1), coeffs(std::make_unique<int[]>(n_rank + 1)) {
+        for (int j = 0; j <= n_rank; j++) {
+            coeffs[j] = (N * first[j] - 2 * first[j-1] - (N - n_rank + 2) * second[j]) / n_rank;
+        }
+    }
     // Returns n-th coefficient of this
     int operator[](unsigned int const &n) const;
     // The rank of this
--- src/kravchuk.cpp
+++ src/kravchuk.cpp
@@ -43,33 +43,34 @@ polynomial get_next_Kravchuk(polynomial const &first, polynomial const &second, 
     return polynomial(next_rank, coeffs);
 }
 
-// Returns all Kravchuk polynomials from rank 0 to rank max_rank, with N fixed
+//  Builds the next Kravchuk polynomial from their recurrence relations. Coefficients only
+std::unique_ptr<int[]> get_next_Kravchuk(std::unique_ptr<int[]> const &first, std::unique_ptr<int[]> const &second, unsigned int const &next_rank, unsigned int const &N) {
+    std::unique_ptr<int[]> coeffs = std::make_unique<int[]>(next_rank);
+    for (int j = 0; j <= next_rank; j++) {
+        coeffs[j] = (N * first[j] - 2 * first[j-1] - (N - next_rank + 2) * second[j]) / next_rank;
+    }
+    return coeffs;
+}
+
+// Returns all Kravchuk polynomials from rank 0 to rank max_rank, with N fixed. Returns at least 2 for optimization
 std::vector<polynomial> get_Kravchuk_pols(unsigned int const &max_rank, unsigned int const &N) {
     std::vector<polynomial> pols;
     pols.reserve(max_rank+1);
-    // Calculates polynomials by thirds, to avoid expensive copying
-    std::unique_ptr<int[]> first_ptr = std::make_unique<int[]>(1);
-    std::unique_ptr<int[]> second_ptr = std::make_unique<int[]>(2);
-    first_ptr[0] = 1;
-    second_ptr[0] = N;
-    second_ptr[1] = -2;
+    std::unique_ptr<int[]> first = std::make_unique<int[]>(max_rank);
+    std::unique_ptr<int[]> second = std::make_unique<int[]>(max_rank);
+    // Set all values to 0 first to avoid any possible issues
+    for (int j = 0; j <= max_rank; j++) {
+        first[0] = second[0] = 0;
+    }
+    first[0] = 1;
+    second[0] = N;
+    second[1] = -2;
+    pols.emplace_back(0, first);
+    pols.emplace_back(1, second);
     unsigned int current_rank = 2;
-    polynomial first(0, first_ptr), second(0, second_ptr);
-    pols.push_back(first);
-    pols.push_back(second);
     while(current_rank <= max_rank) {
-        polynomial third = get_next_Kravchuk(first, second, N);
-        pols.push_back(third);
-        if (current_rank > max_rank) {
-            break;
-        }
-        polynomial first = get_next_Kravchuk(second, third, N);
-        pols.push_back(first);
-        if (current_rank > max_rank) {
-            break;
-        }
-        polynomial second = get_next_Kravchuk(third, first, N);
-        pols.push_back(second);
+        current_rank++;
+        pols.emplace_back(pols[pols.size() - 2], pols[pols.size() - 1], N);
     }
 
     return pols;
