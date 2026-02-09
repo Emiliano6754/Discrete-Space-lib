69ae685e1a1b13ecc7013c94cfea867bfd5e6b29
Emiliano Pastrana <emilianopm93@gmail.com>
2026-02-09 15:04:28 -0600

    Implemented polynomials in 3 variables with addition

--- include/kravchuk.h
+++ include/kravchuk.h
@@ -25,6 +25,30 @@ private:
     std::unique_ptr<double[]> coeffs;
 };
 
+// Polynomial over 3 variables
+class polynomial3{
+public:
+    // Builds a polynomial from its coefficients
+    polynomial3(unsigned int const &rank1, unsigned int const &rank2, unsigned int const &rank3, std::unique_ptr<double[]> const &in_coeffs);
+    // Copy constructor
+    polynomial3(polynomial3 const &other);
+    // Constructs a polynomial over three variables from three polynomials, each over one variable
+    polynomial3(polynomial const &p1, polynomial const &p2, polynomial const &p3);
+    // Returns this evaluated at (m,n,k)
+    double const eval(int const &m, int const &n, int const &k) const;
+    // Returns a reference to the (m,n,k)-th power coefficient of this
+    double& operator()(int const &m, int const &n, int const &k);
+    // Returns a const reference to the (m,n,k)-th power coefficient of this
+    double const& operator()(int const &m, int const &n, int const &k) const;
+    // Adds other to this as polynomials
+    void operator+=(polynomial3 const &other);
+    // Returns a new polynomial, with coefficients equal to the sum of coefficients of both this and other
+    polynomial3 operator+(polynomial3 const &other);
+private:
+    unsigned int n_rank1, n_rank2, n_rank3;
+    std::unique_ptr<double[]> coeffs;
+};
+
 //  Builds the next Kravchuk polynomial from their recurrence relations
 polynomial get_next_Kravchuk(polynomial const &first, polynomial const &second, unsigned int const &N);
 
--- src/kravchuk.cpp
+++ src/kravchuk.cpp
@@ -1,4 +1,4 @@
-#include "Kravchuk.h"
+#include "kravchuk.h"
 #include<iostream>
 
 // Builds a polynomial based on its coefficients
@@ -48,6 +48,120 @@ void polynomial::print() const {
         std::cout << coeffs[j] << "x^" << j << " + ";
     }
     std::cout << "\n";
+}
+
+// Builds a polynomial from its coefficients
+polynomial3::polynomial3(unsigned int const &rank1, unsigned int const &rank2, unsigned int const &rank3, std::unique_ptr<double[]> const &in_coeffs) : n_rank1(rank1), n_rank2(rank2), n_rank3(rank3), coeffs(std::make_unique<double[]>((rank1+1)*(rank2+1)*(rank3+1))) {
+    for (int j = 0; j <= rank3; j++) {
+        for (int k = 0; k <= rank2; k++) {
+            for (int l = 0; l <= rank1; l++) {
+                (*this)(j, k, l) = in_coeffs[l + (k + j * (rank2 + 1)) * (rank1 + 1)];
+            }
+        }
+    }
+}
+
+// Copy constructor
+polynomial3::polynomial3(polynomial3 const &other): n_rank1(other.n_rank1), n_rank2(other.n_rank2), n_rank3(other.n_rank3), coeffs(std::make_unique<double[]>((other.n_rank1 + 1) * (other.n_rank2 + 1) * (other.n_rank3 + 1))) {
+    std::copy(other.coeffs.get(), other.coeffs.get() + (other.n_rank1 + 1) * (other.n_rank2 + 1) * (other.n_rank3 + 1), coeffs.get());
+}
+
+// Constructs a polynomial over three variables from three polynomials, each over one variable
+polynomial3::polynomial3(polynomial const &p1, polynomial const &p2, polynomial const &p3) : n_rank1(p1.rank()), n_rank2(p2.rank()), n_rank3(p3.rank()), coeffs(std::make_unique<double[]>((p1.rank()+1)*(p2.rank()+1)*(p3.rank()+1))) {
+    for (int j = 0; j <= n_rank3; j++) {
+        for (int k = 0; k <= n_rank2; k++) {
+            for (int l = 0; l <= n_rank1; l++) {
+                (*this)(j, k, l) = p1[j] * p2[k] * p3[l];
+            }
+        }
+    }
+}
+
+// Returns this evaluated at (m,n,k)
+double const polynomial3::eval(int const &m, int const &n, int const &k) const {
+    double res = 0;
+    double current_x = 1, current_y = 1, current_z = 1;
+    for (int j = 0; j <= n_rank3; j++) {
+        for (int k = 0; k <= n_rank2; k++) {
+            for (int l = 0; l <= n_rank1; l++) {
+                res += (*this)(j, k, l) * current_x * current_y * current_z;
+                current_x *= m;
+            }
+            current_y *= n;
+        }
+        current_z *= k;
+    }
+    return res;
+}
+
+// Returns a reference to the (m,n,k)-th power coefficient of this
+double& polynomial3::operator()(int const &m, int const &n, int const &k) {
+    return coeffs[m + (n + k * (n_rank2 + 1)) * (n_rank1 + 1)];
+}
+
+// Returns a const reference to the (m,n,k)-th power coefficient of this
+double const& polynomial3::operator()(int const &m, int const &n, int const &k) const {
+    return coeffs[m + (n + k * (n_rank2 + 1)) * (n_rank1 + 1)];
+}
+
+// Adds other to this as polynomials. If this is smaller in any rank to other, a complete copy must be performed in order to address the new compression 
+void polynomial3::operator+=(polynomial3 const &other) {
+    if (other.n_rank1 <= n_rank1 && other.n_rank2 <= n_rank2 && other.n_rank3 <= n_rank3) {
+        for (int j = 0; j <= other.n_rank3; j++) {
+            for (int k = 0; k <= other.n_rank2; k++) {
+                for (int l = 0; l <= other.n_rank1; l++) {
+                    (*this)(j, k, l) += other(j, k, l);
+                }
+            }
+        }
+    } else {
+        const int max_rank1 = std::max(n_rank1, other.n_rank1);
+        const int max_rank2 = std::max(n_rank2, other.n_rank2);
+        const int max_rank3 = std::max(n_rank3, other.n_rank3);
+        std::unique_ptr<double[]> new_coeffs = std::make_unique<double[]>((max_rank1 + 1) * (max_rank2 + 1) * (max_rank3 + 1));
+        unsigned int pos = 0;
+        for (int j = 0; j <= max_rank3; j++) {
+            for (int k = 0; k <= max_rank2; k++) {
+                for (int l = 0; l <= max_rank1; l++) {
+                    pos = l + (k + j * (max_rank2 + 1)) * (max_rank1 + 1);
+                    new_coeffs[pos] = 0;
+                    if (l <= n_rank1 && k <= n_rank2 && j <= n_rank3) {
+                        new_coeffs[pos] += (*this)(j, k, l);
+                    }
+                    if (l <= other.n_rank1 && k <= other.n_rank2 && j <= other.n_rank3) {
+                        new_coeffs[pos] += other(j, k, l);
+                    }
+                }
+            }
+        }
+        coeffs = std::move(new_coeffs);
+        n_rank1 = max_rank1;
+        n_rank2 = max_rank2;
+        n_rank3 = max_rank3;
+    }
+}
+// Returns a new polynomial, with coefficients equal to the sum of coefficients of both this and other
+polynomial3 polynomial3::operator+(polynomial3 const &other) {
+    const int max_rank1 = std::max(n_rank1, other.n_rank1);
+    const int max_rank2 = std::max(n_rank2, other.n_rank2);
+    const int max_rank3 = std::max(n_rank3, other.n_rank3);
+    std::unique_ptr<double[]> new_coeffs = std::make_unique<double[]>((max_rank1 + 1) * (max_rank2 + 1) * (max_rank3 + 1));
+    unsigned int pos = 0;
+    for (int j = 0; j <= max_rank3; j++) {
+        for (int k = 0; k <= max_rank2; k++) {
+            for (int l = 0; l <= max_rank1; l++) {
+                pos = l + (k + j * (max_rank2 + 1)) * (max_rank1 + 1);
+                new_coeffs[pos] = 0;
+                if (l <= n_rank1 && k <= n_rank2 && j <= n_rank3) {
+                    new_coeffs[pos] += coeffs[l + (k + j * (n_rank2 + 1)) * (n_rank1 + 1)];
+                }
+                if (l <= other.n_rank1 && k <= other.n_rank2 && j <= other.n_rank3) {
+                    new_coeffs[pos] += other.coeffs[l + (k + j * (other.n_rank2 + 1)) * (other.n_rank1 + 1)];
+                }
+            }
+        }
+    }
+    return polynomial3(max_rank1, max_rank2, max_rank3, new_coeffs);
 }
 
 //  Builds the next Kravchuk polynomial from their recurrence relations
